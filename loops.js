66168f0ca87eab47a68e16c377fda08938bc46fc
Noah Sragow <nsragow@gmail.com>
2018-12-04 17:49:47 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(array)
+{
+  for(i = 0; i < 25; i++)
+  {
+    array.push(`I am ${i} strange loop${(i === 0)||(i === 1) ? '' : 's'}.`)
+  }
+  return array
+}
+function whileLoop(n)
+{
+  while(n > 0)
+  {
+    console.log(n)
+  }
+}
